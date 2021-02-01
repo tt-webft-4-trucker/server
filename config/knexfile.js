bd85e9f514c2aa5fa13a2337a0ba1b6e62dfc832
Chris <chris@databoxdigital.com>
2021-02-01 12:28:48 -0600

    knex config

--- config/knexfile.js
+++ config/knexfile.js
@@ -22,10 +22,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
   },
