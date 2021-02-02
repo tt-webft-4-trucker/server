000d17fab570d4978ad60a3fd5dd4bd12586d24a
Chris <chris@databoxdigital.com>
2021-02-02 14:14:35 -0600

    return user data on login

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -23,8 +23,10 @@ router.post('/', async (req, res) => {
         profiles[0].password
       );
       if (verifies) {
+        //eslint-disable-next-line
+        const { password, ...profResp } = profiles[0];
         const token = makeJwt(profiles[0]);
-        res.status(200).json({ token: token });
+        res.status(200).json({ profile: profResp, token: token });
       } else
         res.status(401).json({ message: 'Incorrect username or password' });
     })
